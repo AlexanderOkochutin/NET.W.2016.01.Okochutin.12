1a1be1c13d5eb7a247a2f11e03a8624a79bf7588
AlexanderOkochutin <okochutinwork@gmail.com>
2016-11-17 15:14:32 +0300

    fix first task and test for first task

--- Task01.Logic/Fibonacci.cs
+++ Task01.Logic/Fibonacci.cs
@@ -6,15 +6,18 @@ using System.Threading.Tasks;
 
 namespace Task01.Logic
 {
-    public class Fibonacci
+    public static class Fibonacci
     {
         /// <summary>
         /// Iterator for get first N numbers in Fibonacci sequence
         /// </summary>
         /// <param name="n">how many numbers will be calculate</param>
         /// <returns>return next fibonacci number in sequence on each iteration</returns>
+        /// <exception cref="ArgumentOutOfRangeException">throw when input n less or equal zero</exception>
+        /// <exception cref="OverflowException">throws when you want to calculate more than 92 fibonacci number</exception>
         public static IEnumerable<long> GetFirstN(int n)
         {
+            if(n <= 0) throw new ArgumentOutOfRangeException(nameof(n));
             long a = 1;
             long b = 0;
             long c = 0;
---
+++ Task01.NUnitTests/FibonacciTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Task01.Logic;
+namespace Task01.NUnitTests
+{
+    [TestFixture]
+    public class FibonacciTests
+    {
+      
+        [TestCase(0,typeof(ArgumentOutOfRangeException))]
+        [TestCase(-1,typeof(ArgumentOutOfRangeException))]
+        [TestCase(93,typeof(OverflowException))]
+        public void GetFirstN_Exception_Tests(int n,Type exceptionType)
+        {
+            Assert.Throws(exceptionType,()=>ForeachFibonacci(n));
+        }
+
+        public void ForeachFibonacci(int n)
+        {
+            foreach (var num in Fibonacci.GetFirstN(n))
+            {
+                
+            }
+        }
+
+        [TestCase(1, new long[] {0})]
+        [TestCase(2, new long[] { 0,1 })]
+        [TestCase(5, new long[] { 0, 1 ,1,2,3})]
+        [TestCase(10,new long[] { 0, 1 ,1,2,3,5,8,13,21,34})]
+        public void GetFirstN_Tests(int n, long[] expected)
+        {
+            int i = 0;
+            foreach (var num in Fibonacci.GetFirstN(n))
+            {
+                Assert.AreEqual(num,expected[i++]);
+            }
+        }
+
+      
+    }
+}
