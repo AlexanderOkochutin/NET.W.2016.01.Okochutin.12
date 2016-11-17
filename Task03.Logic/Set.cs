fb6933aa8e2f9a843f1d9a2c74e822f743630636
AlexanderOkochutin <okochutinwork@gmail.com>
2016-11-17 16:37:00 +0300

    tests and xml task

--- Task03.Logic/Set.cs
+++ Task03.Logic/Set.cs
@@ -4,20 +4,36 @@ using System.Collections.Generic;
 
 namespace Task03.Logic
 {
+    /// <summary>
+    /// custom generic set
+    /// </summary>
+    /// <typeparam name="T">class which implement IComparable interface</typeparam>
     public class Set<T> : IEnumerable<T> where T : class, IComparable<T>
     {
         private readonly List<T> data = new List<T>();
         public int Count => data.Count;
 
+        /// <summary>
+        /// default constructor
+        /// </summary>
         public Set()
         {
         }
 
+        /// <summary>
+        /// constructor based on collection
+        /// </summary>
+        /// <param name="items">input collection</param>
         public Set(IEnumerable<T> items)
         {
             AddRange(items);
         }
 
+        /// <summary>
+        /// method for adding element in set
+        /// </summary>
+        /// <param name="item"></param>
+        /// <exception cref="InvalidOperationException">when element already exist in set</exception>
         public void Add(T item)
         {
             if (Contains(item))
@@ -27,6 +43,11 @@ namespace Task03.Logic
             data.Add(item);
         }
 
+        /// <summary>
+        /// add some elements in set
+        /// </summary>
+        /// <param name="items"></param>
+        /// <exception cref="InvalidOperationException">when one of input elements already exist in set</exception>
         public void AddRange(IEnumerable<T> items)
         {
             foreach (T item in items)
@@ -35,16 +56,31 @@ namespace Task03.Logic
             }
         }
 
+        /// <summary>
+        /// remove element from set
+        /// </summary>
+        /// <param name="item">removable element</param>
+        /// <returns>true in case when element was in set and false when no such element in set</returns>
         public bool Remove(T item)
         {
             return data.Remove(item);
         }
 
+        /// <summary>
+        /// method which help to know element in set or not
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns>true in case when elem already exist in set and false in other case</returns>
         public bool Contains(T item)
         {
             return data.Contains(item);
         }
 
+        /// <summary>
+        /// union of sets
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns>new set (result of union of two sets)</returns>
         public Set<T> Union(Set<T> other)
         {
             Set<T> result = new Set<T>(data);
@@ -58,6 +94,11 @@ namespace Task03.Logic
             return result;
         }
 
+        /// <summary>
+        /// intersection of two sets
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns>new set (result of intersection of two sets)</returns>
         public Set<T> Intersection(Set<T> other)
         {
             Set<T> result = new Set<T>();
@@ -71,6 +112,11 @@ namespace Task03.Logic
             return result;
         }
 
+        /// <summary>
+        /// difference of teo sets
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns>new set (result of difference of two sets)</returns>
         public Set<T> Difference(Set<T> other)
         {
             Set<T> result = new Set<T>(this);
@@ -81,6 +127,11 @@ namespace Task03.Logic
             return result;
         }
 
+        /// <summary>
+        /// symmetrical difference of two sets
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns>return new set(result of symmetrical difference of two sets)</returns>
         public Set<T> SymmetricalDifference(Set<T> other)
         {
             Set<T> union = Union(other);
--- Task03.NUnitTests/SetTests.cs
+++ Task03.NUnitTests/SetTests.cs
@@ -12,6 +12,78 @@ namespace Task03.NUnitTests
     [TestFixture]
     public class SetTests
     {
+        [TestCase()]
+        public void Constructor_tests()
+        {
+            string[] data = new string[] {"1", "2", "3", "4", "5"};
+            Set<string> test = new Set<string>(data);
+            Set<string> test2 = new Set<string>();
+            test2.Add("1");
+            test2.AddRange(new string[] {"2","3","4","5"});
+            CollectionAssert.AreEqual(test,test2);
+        }
 
+        [TestCase()]
+        public void Remove_Tests()
+        {
+            string[] data1 = new string[] { "1", "2", "3", "4", "5" };
+            string[] data2 = new string[] { "1", "2", "3", "4" };
+            Set<string> test1 = new Set<string>(data1);
+            Set<string> test2 = new Set<string>(data2);
+            test1.Remove("5");
+            CollectionAssert.AreEqual(test1,test2);
+        }
+
+        [TestCase()]
+        public void Union_tests()
+        {
+            string[] data1 = new string[] { "1", "2", "3", "4", "5" };
+            string[] data2 = new string[] {"4","5","6","7" };
+            string[] expectedData = new string[] { "1", "2", "3", "4", "5", "6", "7" };
+            Set<string> test1 = new Set<string>(data1);
+            Set<string> test2 = new Set<string>(data2);
+            Set<string> expected = new Set<string>(expectedData);
+            test1 = test1.Union(test2);
+            CollectionAssert.AreEqual(test1,expected);
+        }
+
+        [TestCase()]
+        public void Intersection_tests()
+        {
+            string[] data1 = new string[] { "1", "2", "3", "4", "5" };
+            string[] data2 = new string[] { "4", "5", "6", "7" };
+            string[] expectedData = new string[] { "4","5" };
+            Set<string> test1 = new Set<string>(data1);
+            Set<string> test2 = new Set<string>(data2);
+            Set<string> expected = new Set<string>(expectedData);
+            test1 = test1.Intersection(test2);
+            CollectionAssert.AreEqual(test1, expected);
+        }
+
+        [TestCase()]
+        public void Difference_tests()
+        {
+            string[] data1 = new string[] { "1", "2", "3", "4", "5" };
+            string[] data2 = new string[] { "4", "5", "6", "7" };
+            string[] expectedData = new string[] { "1", "2", "3" };
+            Set<string> test1 = new Set<string>(data1);
+            Set<string> test2 = new Set<string>(data2);
+            Set<string> expected = new Set<string>(expectedData);
+            test1 = test1.Difference(test2);
+            CollectionAssert.AreEqual(test1, expected);
+        }
+
+        [TestCase()]
+        public void SymDifference_tests()
+        {
+            string[] data1 = new string[] { "1", "2", "3", "4", "5" };
+            string[] data2 = new string[] { "4", "5", "6", "7" };
+            string[] expectedData = new string[] { "1","2","3","6","7"};
+            Set<string> test1 = new Set<string>(data1);
+            Set<string> test2 = new Set<string>(data2);
+            Set<string> expected = new Set<string>(expectedData);
+            test1 = test1.SymmetricalDifference(test2);
+            CollectionAssert.AreEqual(test1, expected);
+        }
     }
 }
